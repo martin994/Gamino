2fe2da871537ea67eb673becbc0b246449762fbd
Martin <mar.inc.987@gmail.com>
2018-11-16 12:14:09 -0300

    6 Pantallas hechas

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/gamino/VerPublicacion.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/gamino/VerPublicacion.java
@@ -3,11 +3,11 @@ package ar.edu.utn.frsf.isi.dam.gamino;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Publicacion extends AppCompatActivity {
+public class VerPublicacion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_publicacion);
+        setContentView(R.layout.activity_ver_publicacion);
     }
 }
